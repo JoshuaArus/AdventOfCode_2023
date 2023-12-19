dba8235bb60910a119d35b81e9826d13548e1832
Joshua Arus <joshua.arus@wildcodeschool.com>
2023-12-19 10:00:32 +0100

    Project creation

---
+++ Day.cs
@@ -0,0 +1,28 @@
+﻿namespace AdventOfCode_2023
+{
+    public abstract class Day
+    {
+        protected string[] inputs;
+
+        public Day()
+        {
+            string inputName = this.GetType().Name.Replace("Day", "Input") + ".txt";
+            try
+            {
+                //read string from file
+                inputs = File.ReadAllLines(inputName);
+                FirstPart();
+                SecondPart();
+            } catch (FileNotFoundException e)
+            {
+                Console.WriteLine($"Input {inputName} doesn't exist");
+            } catch (NotSupportedException)
+            {
+                //nvm, FirstPart or SecondPart not implemented yet
+            }
+        }
+
+        public abstract void SecondPart();
+        public abstract void FirstPart();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode_2023
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string classRegex = @"^AdventOfCode_2023\.Day_[0-9]{1,2}$";
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            foreach (
+                string classe in
+                    Assembly
+                    .GetExecutingAssembly()
+                    .GetTypes()
+                    .Select(t => t.FullName)
+                    .Where(s => Regex.IsMatch(s, classRegex)))
+            {
+                try
+                {
+                    Console.WriteLine(classe.Split('.').Last().ToUpper());
+                    Activator.CreateInstance(null, classe);
+                }
+                catch (Exception)
+                {
+                    //nvw
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
