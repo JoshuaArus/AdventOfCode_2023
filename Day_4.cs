2054b61101b4e001d69898f44b1ccf9c02a53168
Joshua Arus <joshua.arus@wildcodeschool.com>
2023-12-19 16:24:31 +0100

    day 4

---
+++ Day_4.cs
@@ -0,0 +1,69 @@
+﻿
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode_2023
+{
+    public class Day_4 : Day
+    {
+        public override void FirstPart()
+        {
+            double result = 0;
+
+            foreach (string input in inputs)
+            {
+                Match match = Regex.Match(input, @"Card[ ]*(\d{1,}): (.*)");
+                int cardId = int.Parse(match.Groups[1].Value);
+
+                string[] cardNumbers = match.Groups[2].Value.Split(" | ");
+                List<string> winningNumbers = cardNumbers[0].Split(" ").Select(n => n.Trim()).Where(n => !string.IsNullOrWhiteSpace(n)).ToList();
+                List<string> ownedNumbers = cardNumbers[1].Split(" ").Select(n => n.Trim()).Where(n => !string.IsNullOrWhiteSpace(n)).ToList();
+                
+                int matchingNumbers = ownedNumbers.Count(n => winningNumbers.Contains(n));
+
+                if (matchingNumbers > 0)
+                {
+                    result += Math.Pow(2, matchingNumbers - 1);
+                }
+
+            }
+
+            Console.WriteLine($"FirstPart : {result}");
+        }
+
+        public override void SecondPart()
+        {
+            double result = 0;
+
+            IDictionary<int, int> cardCopiesById = Enumerable.Range(1, inputs.Length).ToDictionary(i => i, i => 1);
+
+            foreach (string input in inputs)
+            {
+                Match match = Regex.Match(input, @"Card[ ]*(\d{1,}): (.*)");
+                int cardId = int.Parse(match.Groups[1].Value);
+
+                string[] cardNumbers = match.Groups[2].Value.Split(" | ");
+                List<string> winningNumbers = cardNumbers[0].Split(" ").Select(n => n.Trim()).Where(n => !string.IsNullOrWhiteSpace(n)).ToList();
+                List<string> ownedNumbers = cardNumbers[1].Split(" ").Select(n => n.Trim()).Where(n => !string.IsNullOrWhiteSpace(n)).ToList();
+
+                int matchingNumbers = ownedNumbers.Count(n => winningNumbers.Contains(n));
+
+                if (matchingNumbers > 0)
+                {
+                    for (int i = 1; i <= matchingNumbers; i++)
+                    {
+                        cardCopiesById[cardId + i] = cardCopiesById[cardId + i] + cardCopiesById[cardId];
+                    }
+                }
+            }
+
+            foreach(var kvp in cardCopiesById)
+            {
+                result += kvp.Value;
+            }
+
+            
+
+            Console.WriteLine($"SecondPart : {result}");
+        }
+    }
+}
